ec33ea8e0c56a6a1df494332d1cdfb4e298b1b53
Abhishek Pramanik <abhishekpramanikk21@gmail.com>
2023-09-09 11:17:16 +0530

    Colour Changer

---
+++ script.js
@@ -0,0 +1,40 @@
+function changebackground() {
+	document.body.style.backgroundColor = 'white';
+}
+
+function changebackground2() {
+	document.body.style.backgroundColor = 'black';
+}
+
+function changebackground3() {
+	document.body.style.backgroundColor = 'red';
+}
+
+function changebackground4() {
+	document.body.style.backgroundColor = 'green';
+}
+
+function changebackground5() {
+	document.body.style.backgroundColor = 'blue';
+}
+
+function changebackground6() {
+	document.body.style.backgroundColor = 'yellow';
+}
+
+function changebackground7() {
+	document.body.style.backgroundColor = 'orange';
+}
+
+function changebackground8() {
+	document.body.style.backgroundColor = 'purple';
+}
+
+
+function changebackground9() {
+	document.body.style.backgroundColor = '#ff69b4';
+}
+
+function changebackground10() {
+	document.body.style.backgroundColor = 'grey';
+}
